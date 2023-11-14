563ea6a84e3e3b0db220ff684ca656acf6da081f
AlpAcA0072 <AlpAcA0072@gmail.com>
2023-11-14 10:31:48 +0800

    new version

--- evoxbench/test_suites/citymop.py
+++ evoxbench/test_suites/citymop.py
@@ -2,41 +2,40 @@ from evoxbench.benchmarks import MoSegNASBenchmark
 
 __all__ = ["citymop"]
 
-# TODO: 需要一个硬件相关的指标（模型采集 功耗，是否指定GPU？）
 # params | latency | FPS | flops | mIoU
 def citymop(problem_id):
     if problem_id == 1:
-        # Balance the speed and accuracy of the model.
-        return MoSegNASBenchmark(objs="FPS&mIoU", normalized_objectives=False)
+        # Balance the accuracy and reference speed.
+        return MoSegNASBenchmark(objs="mIoU&FPS", normalized_objectives=False)
     elif problem_id == 2:
-        # Balance the consumption and accuracy of the model.
+        # Balance the accuracy and consumption of the model.
         return MoSegNASBenchmark(
-            objs="flops&mIoU", normalized_objectives=False
+            objs="mIoU&flops", normalized_objectives=False
         )
     elif problem_id == 3:
-        # Find the relationship between size and accuracy of the model.
+        # Find the relationship between accuracy and size of the model.
         return MoSegNASBenchmark(
-            objs="params&mIoU", normalized_objectives=False
+            objs="mIoU&params", normalized_objectives=False
         )
     elif problem_id == 4:
         # Find the relationship between size and speed of the model (including accuracy).
         return MoSegNASBenchmark(
-            objs="latency&params&mIoU", normalized_objectives=False
+            objs="mIoU&latency&params", normalized_objectives=False
         )
     elif problem_id == 5:
         # Find the relationship between speed and consumption of the model (including accuracy).
         return MoSegNASBenchmark(
-            objs="latency&flops&mIoU", normalized_objectives=False
+            objs="mIoU&latency&flops", normalized_objectives=False
         )
     elif problem_id == 6:
-        # Find the relationship between size, consumption and accuracy.
+        # Find the relationship between accuracy, size and consumption.
         return MoSegNASBenchmark(
-            objs="params&flops&mIoU", normalized_objectives=False
+            objs="mIoU&params&flops", normalized_objectives=False
         )
     elif problem_id == 7:
-        # Balance all the hyperparameters.
+        # Balance all the objectives.
         return MoSegNASBenchmark(
-            objs="params&flops&latency&mIoU", normalized_objectives=False
+            objs="mIoU&params&flops&latency", normalized_objectives=False
         )
     else:
         raise ValueError("the requested problem id does not exist")
